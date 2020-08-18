d1ce78cc89884d50f819ff56158ad6aae308da6a
Harsh Desai <harsh@portworx.com>
2020-08-18 11:09:21 -0700

    Add CRD for ActionApproval (#8)
    * Add CRD for ActionApproval
    
    - remove approval fields from AutopilotRuleobject
    
    This CRD has following benefits
    
    - Allows multiple action approval workflows per object. Previously, a single CRD object had a list of approvals.
    - Decouples approval from the AutopilotRuleObject whose main purpose was to just track status of all events per object
    - Conforms to the multi-tenancy model as it's a namespaced resource
    
    Signed-off-by: Harsh Desai <harsh@portworx.com>
    
    * review feedback
    
    - promote expected result as a dedicated struct
    - consolidate approval variables to one file
    
    Signed-off-by: Harsh Desai <harsh@portworx.com>

--- pkg/client/informers/externalversions/autopilot/v1alpha1/interface.go
+++ pkg/client/informers/externalversions/autopilot/v1alpha1/interface.go
@@ -24,6 +24,8 @@ import (
 
 // Interface provides access to all the informers in this group version.
 type Interface interface {
+	// ActionApprovals returns a ActionApprovalInformer.
+	ActionApprovals() ActionApprovalInformer
 	// AutopilotRules returns a AutopilotRuleInformer.
 	AutopilotRules() AutopilotRuleInformer
 	// AutopilotRuleObjects returns a AutopilotRuleObjectInformer.
@@ -39,6 +41,11 @@ type version struct {
 // New returns a new Interface.
 func New(f internalinterfaces.SharedInformerFactory, namespace string, tweakListOptions internalinterfaces.TweakListOptionsFunc) Interface {
 	return &version{factory: f, namespace: namespace, tweakListOptions: tweakListOptions}
+}
+
+// ActionApprovals returns a ActionApprovalInformer.
+func (v *version) ActionApprovals() ActionApprovalInformer {
+	return &actionApprovalInformer{factory: v.factory, namespace: v.namespace, tweakListOptions: v.tweakListOptions}
 }
 
 // AutopilotRules returns a AutopilotRuleInformer.
